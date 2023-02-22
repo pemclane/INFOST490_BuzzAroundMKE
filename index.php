950f862192c2a7b90342a909147bb3077069c2c6
pemclane <103152248+pemclane@users.noreply.github.com>
2023-02-22 21:02:55 +0000

    Add files via upload
    Initial index.php page

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Buzz Around MKE</title>
+</head>
+
+<style>
+.center{
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+  width: 50%;
+}
+
+body{
+	background:#215F97;
+}
+
+h1{
+  color:#E9BA25;
+}
+
+</style>
+
+<body>
+<h1 align="center"> Buzz Around MKE is coming soon!</h1>
+<br>
+<img src="buzz_logo.png" alt="Buzz Around logo" class="center">
+</body>
+
+</html>
